2b7d6cebeb9e03fdd42e8ceee6e63b488b87880b
Grzegorz Koszczał <grzegorzkoszczal@gmail.com>
2024-01-30 13:22:03 +0100

    Update

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-### Autoblogger for resume
+# Autoblogger for resume
+
+## Purpose of this repository
+
+The main goal of this repo is to showcase a simple applications that incorporates the use of generative nature of ChatGPT. At first, the user provides the URL to a YouTube video that comes with the subtitles, such as music video, vlog etc. and executes prompts based on the context of the transcript.
+
+## Installation
+
+### Downloading the repository
+
+In order to download the entire repo, clone it in the chosen directory
+```console
+foo@bar:~$ git clone https://github.com/grzegorzkoszczal/autoblogger.git
+```
+
+### Installing environment
+In order to install all the needed stuff for further development of the code, use the Install.sh script
+```console
+foo@bar:~$ source install.sh
+```
+It will automatically create Python virtual environment, activate it and pull all the dependecies required
+for the development.
--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 # Autoblogger for resume
 
-# What to do:
-# 1. Parse the URL to a YouTube video as a user input
-#    I) Check for user input validation
-#    II) Create a really simple GUI for a URL parsing
-# 2. Download the subtitles from the video and save them as a text file.
-#    I) Show the subtitles taken from a video.
-# 3. Parse the wall-of-text to the ChatGPT and ask it to create a post on
-#    a blog based on that
-# 4. Check the error handling, correct code writing conventions, comments,
-#    and nice set up of the project on the GitHub
-#    I) A good idea would be a script that automatically creates Python
-#    virtual environment and pulls the dependencies.
-
 # Test cases below:
 # Ozzy Osbourne - A Thousand Shades
 # https://www.youtube.com/watch?v=ZNVs-dfFUj0
@@ -21,29 +8,9 @@
 
 
 
-
-
-# README.md 
-# 1. Explain the project
-
-# download subtitles function - explain what it does
-# provide screenshots on how does the raw transcript looks like
-
-# manage subtitles function - explain what it does
-# provide screenshots on how does the prepared input looks like
-
-
-
-
-import gui
-import os
-import time
-import re
 from openai import OpenAI
-import pandas as pd
 from youtube_transcript_api import YouTubeTranscriptApi
 from tkinter import *
-from tkinter import messagebox
 from functools import partial
 
 
@@ -58,102 +25,79 @@ def get_api_key():
                 key = line
         print("API key retrieved successfully")
         return key
+    
     except Exception as e:
         print(f"An error occurred while retrieving API key: {e}")
 
 
 # This function takes the user input (YouTube video URL), downloads the
-# subtitles of a video, and saves it in "subtitles.txt" file
+# subtitles of a video, and saves it in "subtitles" global variable
 def download_subtitles(entry):
     try:
         url = entry.get()
-        print("URL entered: ", url)
         youtube_id = url.split("https://www.youtube.com/watch?v=", 1)[1]
-        output_file = "subtitles.txt"
         # assigning srt variable with the list 
         # of dictionaries obtained by the get_transcript() function
         srt = YouTubeTranscriptApi.get_transcript(youtube_id,
                                                     languages=['en'])
         
+        global subtitles
+        subtitles = str()
 
-        # creating or overwriting a file "subtitles.txt" with 
-        # the info inside the context manager
-        with open("subtitles.txt", "w") as f:
-        
-            # iterating through each element of list srt
-            for i in srt:
-                # writing each element of srt on a new line
-                f.write("{}\n".format(i))
-
-            print(f'Subtitles downloaded successfully and saved to {output_file}')
-        manage_subtitles()
+        for key in srt:
+            subtitles = subtitles + key["text"]
+            subtitles = subtitles.replace("♪ ","")
+            subtitles = subtitles.replace(" ♪","")
+            subtitles = subtitles.replace("\\n","")
+            subtitles = subtitles + ". "
 
     except Exception as e:
         print(f'An error occurred while downloading the subtitles: {e}')
 
 
-# This function prepares downloaded subtitles to be easier to read as an input
-# for a prompt, that will be used for ChatGPT
-def manage_subtitles():
-    try:
-        delimiters = "{\'text\': \'", "{'text': \"", "', 'start'", "\", 'start'"
-        regex_pattern = '|'.join(map(re.escape, delimiters))
+def chatgpt(window, radio_button_x, api_key, answer_box):
+    if answer_box.compare("end-1c", "!=", "1.0"):
+        answer_box.delete("1.0", END)
 
-        with open("input.txt", "w") as input:
-            with open("subtitles.txt", "r") as f:
-                for line in f:
-                    result = re.split(regex_pattern, line)[1]
-                    result = result.replace("♪ ","")
-                    result = result.replace(" ♪","")
-                    result = result.replace("\\n"," ")
-                    input.write(result+". ")
-                    # print(result)
+    answer_box.insert(END,"Thinking...")
+    window.update()
 
-    except Exception as e:
-        print(f'An error occurred while managing the subtitles: {e}')
-
-
-def chatgpt(radio_button_x, api_key, answer):
     which_prompt = radio_button_x.get()
-    # radio_button_requests = [
-    #     "Based on the subtitles of the video, write a short blog entry about the topic of a video: ",
-    #     "Describe the emotion that are shown in lyrics of this song: ",
-    #     "Given the subtitles of a video, guess what is the topic of it: "
-    # ]
     prompts = {
         0: "Write a blog entry based on video subtitles, presented in double quotation marks: ",
-        1: "Explain emotions in song lyrics, presented in double quotation marks: ",
-        2: "Guess what is this video about, based on these subtitles presented in double quotation marks: : "
+        1: "Describe emotions in song lyrics, presented in double quotation marks: ",
+        2: "Guess what is this video about, based on these subtitles presented in double quotation marks: "
     }
-    # https://www.youtube.com/watch?v=ZNVs-dfFUj0
-    with open("input.txt", "r") as f:
-        subtitles = f.readline()
     user_command = prompts.get(which_prompt)
-    # print("User: "+user_command+"\""+subtitles+"\"")
+    user_command = "User: "+user_command+"\""+subtitles+"\""
+    print(user_command)
 
-    # ChatGPT part
+    # Implementing responses from ChatGPT
     client = OpenAI(api_key=api_key)
-    messages = [ {"role": "system", "content": "You are a intelligent assistant."} ]
-    message = user_command
-    if message: 
-        messages.append({"role": "user", "content": message},)
-        chat = client.chat.completions.create(model="gpt-3.5-turbo", messages=messages) 
-        
-    reply = chat.choices[0].message.content 
-    # print(f"ChatGPT: {reply}") 
-    answer.insert(END,reply)
-    messages.append({"role": "assistant", "content": reply})
+    MODEL = "gpt-3.5-turbo"
+    response = client.chat.completions.create(
+        model=MODEL,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": user_command},
+        ],
+        temperature=0,
+    )
+    final_message_from_ChatGPT = response.choices[0].message.content
+    answer_box.delete("1.0", END)
+    answer_box.insert(END,final_message_from_ChatGPT)
 
 
 # Here is created a simple graphical layout of an application
-def window(api_key):
+def menu(api_key):
 
 
     # Create GUI window
     window = Tk()
-    window.geometry("1280x720")
+    window.geometry("800x800")
     window.title("Autoblogger")
     window.config(background="black")
+
 
     # Information what to do in app window
     label1 = Label(window,
@@ -163,11 +107,13 @@ def window(api_key):
                   bg="black")
     label1.pack(pady=10)
 
+
     # Text field - paste the URL
     entry = Entry(window,
-                    width="40",
+                    width=44,
                     font=("Arial", 10))
     entry.pack(pady=10)
+
 
     # Button - confirm download of subtitles from URL
     button_download = Button(window,
@@ -176,20 +122,19 @@ def window(api_key):
                              font=("Arial", 20, "bold"),
                              fg="black",
                              bg="white",
-                             width=30
+                             width=20
                     )
     button_download.pack(pady=10)
+    
 
-    # Blog entry based on subtitles
-    # Explain emotions in song lyrics
-    # Guess what is this video about
-
+    # Labels of prompts to choose
     radio_button_x = IntVar()
     radio_button_requests = [
         "Blog entry based on subtitles",
         "Explain emotions in song lyrics",
         "Guess what is this video about"
     ]
+
 
     # Text that suggests You to choose the task You want to give to ChatGPT
     label2 = Label(window,
@@ -211,35 +156,53 @@ def window(api_key):
                                    bg="white",
                                    width=30)
         radio_button.pack(pady=5, anchor=CENTER)
+    
 
 
     # A field with an answer given back by ChatGPT
-    answer = Text(window,
+    answer_box = Text(window,
                   name="text field",
                   background="white",
-                  height=15,
+                  height=22,
                   width=100,)
+
 
     # Button - ask ChatGPT
     button_prompt = Button(window,
                            text="Pass the prompt",
-                           command=partial(chatgpt, radio_button_x, api_key, answer),
+                           command=partial(chatgpt, window, radio_button_x, api_key, answer_box),
                            font=("Arial", 20, "bold"),
                            fg="black",
                            bg="white",
-                           width=30
+                           width=20
                     )
     button_prompt.pack(pady=30)
-    answer.pack()
+    answer_box.pack()
+
+
+    # Text that indicates place to put ChatGPT secret API key
+    label3 = Label(window,
+                   anchor=W,
+                   justify=LEFT,
+                   text="Pass the ChatGPT secret API key: ",
+                   font=("Arial", 10, "bold"),
+                   fg="white",
+                   bg="black")
+    # label3.pack(padx=50, side=LEFT)
+
+
+    # Text field - paste the ChatGPT secret API key
+    entry2 = Entry(window,
+                    width=200,
+                    font=("Arial", 10))
+    # entry2.pack(padx=50, side=RIGHT)
 
     window.mainloop()
 
 
 def main():
     api_key = get_api_key()
-    window(api_key)
-
-    # gui.window()
+    menu(api_key)
 
 
 if __name__ == "__main__":
